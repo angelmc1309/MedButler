7ecf01b14aaa2bd7cf4919cb1084e7412f9f62f0
KChitose <queensfelixqueensblade@gmail.com>
2020-03-21 16:16:29 +0100

    Sprint 1: LAY1 Arreglat i he afegit elements nous.

--- app/src/main/java/com/example/medbutler/Sign_in.kt
+++ app/src/main/java/com/example/medbutler/Sign_in.kt
@@ -1,9 +1,12 @@
 package com.example.medbutler
 
+import android.app.DatePickerDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_sign_in.*
+import java.util.*
 
 class Sign_in : AppCompatActivity() {
 
@@ -15,5 +18,33 @@ class Sign_in : AppCompatActivity() {
         val intent= Intent(this, login::class.java)
         startActivity(intent)
     }
+    fun pickBirthday(view: View){
+        val calendari=Calendar.getInstance()
+        val any=calendari.get(Calendar.YEAR)
+        val mes=calendari.get(Calendar.MONTH)
+        val dia=calendari.get(Calendar.DAY_OF_MONTH)
+        val mostrarDate=DatePickerDialog(this, DatePickerDialog.OnDateSetListener{view, lany, lmes, ldia -> birthdayRegist.setText(""+ldia+"/"+(lmes.toInt()+1).toString()+"/"+lany)} , any, mes, dia)
+        mostrarDate.show()
+    }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
