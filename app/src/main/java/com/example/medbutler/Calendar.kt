d56c1a556dc7344850e69693da24a5ddad537eab
angelmc1309 <angelmc1309@gmail.com>
2020-03-22 17:13:25 +0100

    INT 3: link entre calendar i dayLayout

--- app/src/main/java/com/example/medbutler/Calendar.kt
+++ app/src/main/java/com/example/medbutler/Calendar.kt
@@ -5,15 +5,24 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.view.View
+import android.widget.CalendarView
 
 class Calendar : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.calendar_layout)
+        val cal = findViewById<CalendarView>(R.id.calendar_view)
+        cal.setOnDateChangeListener(CalendarView.OnDateChangeListener { view, year, month, dayOfMonth -> calendarClick(cal)})
     }
     fun actionUser(view: View){
         val intent= Intent(this, UserProfile::class.java)
         startActivity(intent)
+    }
+    fun calendarClick(view: View){
+        val intent= Intent(this, DayActivity::class.java)
+        startActivity(intent)
+
     }
 }
---
+++ app/src/main/java/com/example/medbutler/DayActivity.kt
@@ -0,0 +1,12 @@
+package com.example.medbutler
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DayActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.day_layout)
+    }
+}
