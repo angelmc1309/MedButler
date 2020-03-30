b91a53168d791c8677291530238f4e4e41d85bc6
KChitose <queensfelixqueensblade@gmail.com>
2020-03-31 00:09:49 +0200

    Spring2-3 combinat IMP3: He creat la classe usuari. FBS: firebase setup fet. Sign up/in(firebase): fet.

--- app/src/main/java/com/example/medbutler/Sign_up.kt
+++ app/src/main/java/com/example/medbutler/Sign_up.kt
@@ -4,20 +4,26 @@ import android.app.DatePickerDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
 import android.view.View
+import android.widget.EditText
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthException
 import kotlinx.android.synthetic.main.activity_sign_up.*
-import java.util.*
 import java.util.Calendar
 
 class Sign_up : AppCompatActivity() {
 
+    private lateinit var firebase_auth: FirebaseAuth;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+        firebase_auth=FirebaseAuth.getInstance()
     }
     fun actionGoToLogin(view: View){
-        val intent= Intent(this, login::class.java)
+        val intent= Intent(this, Login::class.java)
         startActivity(intent)
     }
     fun pickBirthday(view: View){
@@ -29,9 +35,54 @@ class Sign_up : AppCompatActivity() {
         mostrarDate.show()
     }
     fun enregistrar(view: View){
-        Toast.makeText(this,"Encara no pots enregistrar!", Toast.LENGTH_SHORT).show()
-        val intent= Intent(this, login::class.java)
-        startActivity(intent)
+
+
+        if (! passwordRegist1.text.toString().equals(passwordRegist2.text.toString())){
+            Toast.makeText(this,"Contresenya diferent", Toast.LENGTH_SHORT).show()
+        }
+        if(passwordRegist1.text.toString().isEmpty()){
+            passwordRegist1.error="Camp obligatori!"
+            passwordRegist1.requestFocus()
+            return
+        }
+        if(passwordRegist2.text.toString().isEmpty()){
+            passwordRegist2.error="Camp obligatori!"
+            passwordRegist2.requestFocus()
+            return
+        }
+        if(passwordRegist1.length()<6){
+            passwordRegist1.error="Mínim 6 caracters!"
+            passwordRegist1.requestFocus()
+            return
+        }
+        if(birthdayRegist.toString().isEmpty()){
+            birthdayRegist.error="Camp obligatori"
+            birthdayRegist.requestFocus()
+            return
+        }
+        if(radioGroupGender.checkedRadioButtonId==-1){
+            Toast.makeText(this, "Ets un crack!",Toast.LENGTH_SHORT).show()
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(usernameRegist.text.toString()).matches()){
+            usernameRegist.error="Ha de ser un email!"
+            usernameRegist.requestFocus()
+            return
+
+        }
+        firebase_auth.createUserWithEmailAndPassword(usernameRegist.text.toString(),passwordRegist1.text.toString())
+            .addOnCompleteListener(this){ task->
+                if(task.isSuccessful){
+                    val intent= Intent(this, Login::class.java)
+                    startActivity(intent)
+                    finish()
+                }else{
+                    var e:FirebaseAuthException= task.exception as FirebaseAuthException
+
+                    Toast.makeText(baseContext,e.message.toString(),Toast.LENGTH_LONG).show()
+
+                }
+            }
+
 
 
     }
