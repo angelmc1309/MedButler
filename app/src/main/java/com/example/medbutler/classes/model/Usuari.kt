76fe53fa780a5fd2b05cf46403c2af09bd90b452
angelmc1309 <angelmc1309@gmail.com>
2020-04-05 19:02:35 +0200

    IMP 2: classes per modificar i guardar dades. TEST 2:comprovat que les funcionalitats implementades funcionen. Falta gestionar notificacions i vincular amb vista

--- app/src/main/java/com/example/medbutler/classes/model/Usuari.kt
+++ app/src/main/java/com/example/medbutler/classes/model/Usuari.kt
@@ -1,19 +1,35 @@
 package com.example.medbutler.classes.model
 
 open class Usuari {
-    private lateinit var username:String
-    private lateinit var passwrd:String
-    private lateinit var birthday:String
-    private lateinit var gender:String
+    private var username:String
+    private var passwrd:String
+    private var birthday:String
+    private var gender:String
+    private val medList:MedList = MedList()
     constructor(username:String,password:String, birthday:String, gender:String){
         this.username=username
         this.passwrd=password
         this.birthday=birthday
         this.gender=gender
     }
+
     fun getId():String{
         return "test"
     }
+
+    fun addMed(id: String,name: String, period: Int,duration: Int,startTimeMinute:Int,
+               startTimeHour:Int,allowNotification:Boolean){
+        medList.addMed(id, name, period, duration, startTimeMinute, startTimeHour, allowNotification)
+    }
+
+    fun getMedList():String {
+        return medList.toString()
+    }
+
+    fun removeMed(id: String) {
+        medList.removeMed(id)
+    }
     //var d=Usuari("k","j","s","s")
+
 
 }
