f3777a86ddb7f37fb12b34dac611693387893102
Eduard <eduvives2212@gmail.com>
2020-06-06 13:09:36 +0200

    FAQs, Privacy Policy i Terms & Conditions personalitzats per MedButler afegits amb Dialog

--- app/src/main/java/com/example/medbutler/classes/view/SettingsHelpSupportActivity.kt
+++ app/src/main/java/com/example/medbutler/classes/view/SettingsHelpSupportActivity.kt
@@ -1,7 +1,9 @@
 package com.example.medbutler.classes.view
 
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.example.medbutler.R
 
@@ -19,10 +21,42 @@ class SettingsHelpSupportActivity : AppCompatActivity() {
             .commit()
     }
 
-    class HelpSupportSettingsFragment : PreferenceFragmentCompat() {
+    class HelpSupportSettingsFragment : PreferenceFragmentCompat(), Preference.OnPreferenceClickListener {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             // Load the preferences from an XML resource
             setPreferencesFromResource(R.xml.help_support_preferences, rootKey)
+
+            findPreference(getString(R.string.key_faq)).onPreferenceClickListener = this
+        }
+
+        override fun onPreferenceClick(preference: Preference?): Boolean {
+            if (preference?.key.equals("key_faq")){
+                openDialogFAQ();
+            }
+            return true
+        }
+
+        private fun openDialogFAQ() {
+            // Initialize a new instance of
+            val builder = AlertDialog.Builder(this@HelpSupportSettingsFragment.activity!!)
+
+            // Set the alert dialog title
+            builder.setTitle("FAQ")
+
+            // Display a message on alert dialog
+            builder.setMessage(getText(R.string.faqs_english))
+
+            // Set a positive button and its click listener on alert dialog
+            builder.setPositiveButton("Close"){dialog, which ->
+                // Do something when user press the positive button
+
+            }
+
+            // Finally, make the alert dialog using builder
+            val dialog: AlertDialog = builder.create()
+
+            // Display the alert dialog on app interface
+            dialog.show()
         }
     }
 }
--- app/src/main/java/com/example/medbutler/classes/view/SettingsPrivacySecurityActivity.kt
+++ app/src/main/java/com/example/medbutler/classes/view/SettingsPrivacySecurityActivity.kt
@@ -1,7 +1,9 @@
 package com.example.medbutler.classes.view
 
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.example.medbutler.R
 
@@ -19,10 +21,68 @@ class SettingsPrivacySecurityActivity : AppCompatActivity() {
             .commit()
     }
 
-    class PrivacySecuritySettingsFragment : PreferenceFragmentCompat() {
+    class PrivacySecuritySettingsFragment : PreferenceFragmentCompat(), Preference.OnPreferenceClickListener {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             // Load the preferences from an XML resource
             setPreferencesFromResource(R.xml.privacy_security_preferences, rootKey)
+
+            findPreference(getString(R.string.key_privacy_policy)).onPreferenceClickListener = this
+            findPreference(getString(R.string.key_terms_conditions)).onPreferenceClickListener = this
+        }
+
+        override fun onPreferenceClick(preference: Preference?): Boolean {
+            if (preference?.key.equals("key_privacy_policy")){
+                openDialogPrivacyPolicy();
+            } else if (preference?.key.equals("key_terms_conditions")){
+                openDialogTermsConditions();
+            }
+            return true
+        }
+
+        private fun openDialogPrivacyPolicy() {
+            // Initialize a new instance of
+            val builder = AlertDialog.Builder(this@PrivacySecuritySettingsFragment.activity!!)
+
+            // Set the alert dialog title
+            builder.setTitle("Privacy Policy")
+
+            // Display a message on alert dialog
+            builder.setMessage(getText(R.string.privacy_policy))
+
+            // Set a positive button and its click listener on alert dialog
+            builder.setPositiveButton("Close"){dialog, which ->
+                // Do something when user press the positive button
+
+            }
+
+            // Finally, make the alert dialog using builder
+            val dialog: AlertDialog = builder.create()
+
+            // Display the alert dialog on app interface
+            dialog.show()
+        }
+
+        private fun openDialogTermsConditions() {
+            // Initialize a new instance of
+            val builder = AlertDialog.Builder(this@PrivacySecuritySettingsFragment.activity!!)
+
+            // Set the alert dialog title
+            builder.setTitle("Terms & Conditions")
+
+            // Display a message on alert dialog
+            builder.setMessage(getText(R.string.terms_and_conditions))
+
+            // Set a positive button and its click listener on alert dialog
+            builder.setPositiveButton("Close"){dialog, which ->
+                // Do something when user press the positive button
+
+            }
+
+            // Finally, make the alert dialog using builder
+            val dialog: AlertDialog = builder.create()
+
+            // Display the alert dialog on app interface
+            dialog.show()
         }
     }
 }
